16a9aac375f3a061a9f1192109b3417e42754677
PureGero <puregero@gmail.com>
2021-11-13 06:49:01 +0000

    Break after rendering one portal

--- src/main/java/me/puregero/borderportals/PortalRendererListener.java
+++ src/main/java/me/puregero/borderportals/PortalRendererListener.java
@@ -103,6 +103,8 @@ public class PortalRendererListener implements Listener {
                             currentRenderedBlocks.add(block);
                         }
                     }
+                    
+                    break;
                 }
             }
         }
