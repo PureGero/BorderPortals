3e26caff1a65972fe74d7974143e4d374c9f1729
PureGero <puregero@gmail.com>
2021-11-13 04:06:02 +0000

    Fix orientation and add signs

--- src/main/java/me/puregero/borderportals/PortalRendererListener.java
+++ src/main/java/me/puregero/borderportals/PortalRendererListener.java
@@ -1,8 +1,13 @@
 package me.puregero.borderportals;
 
+import org.bukkit.Axis;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
+import org.bukkit.block.BlockFace;
+import org.bukkit.block.data.BlockData;
+import org.bukkit.block.data.Orientable;
+import org.bukkit.block.data.type.Sign;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -13,6 +18,11 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class PortalRendererListener implements Listener {
+
+    private static final int[] EAST = new int[] { 1, 0 };
+    private static final int[] WEST = new int[] { -1, 0 };
+    private static final int[] NORTH = new int[] { 0, -1 };
+    private static final int[] SOUTH = new int[] { 0, 1 };
 
     private final BorderPortals borderPortals;
 
@@ -30,7 +40,7 @@ public class PortalRendererListener implements Listener {
             currentRenderedBlocks.clear();
 
             String myServer = borderPortals.getServerAt(event.getTo());
-            for (int[] dir : new int[][]{ {1,0}, {-1,0}, {0,1}, {0,-1} }) {
+            for (int[] dir : new int[][]{ EAST, WEST, NORTH, SOUTH }) {
                 int x = dir[0] * 5;
                 int z = dir[1] * 5;
                 String server = borderPortals.getServerAt(event.getTo().clone().add(x, 0, z));
@@ -68,13 +78,22 @@ public class PortalRendererListener implements Listener {
         int dz = dir[1];
 
         for (int i = 0; i <= 5; i++) {
-            if (!stringEquals(borderPortals.getServerAt(location.clone().add(dx * i, 0, dz * i)), myServer)) {
+            String server = borderPortals.getServerAt(location.clone().add(dx * i, 0, dz * i));
+            if (!stringEquals(server, myServer)) {
+                Location signLoc = location.clone().add(dx * i, 1, dz * i);
+                Block signBlock = signLoc.getBlock();
+                if (!signBlock.getType().isOccluding()) {
+                    player.sendBlockChange(signLoc, createWallSign(dir));
+                    player.sendSignChange(signLoc, new String[]{ "Teleport to", "server", server });
+                    currentRenderedBlocks.add(signBlock);
+                }
+
                 for (int j = -5 + i; j <= 5 - i; j++) {
                     for (int k = -5 + i + Math.abs(j); k <= 5 - i - Math.abs(j); k++) {
                         Location loc = location.clone().add(dx * i + dz * j, k + 1, dz * i + dx * j);
                         Block block = loc.getBlock();
                         if (!block.getType().isOccluding()) {
-                            player.sendBlockChange(loc, Material.NETHER_PORTAL.createBlockData());
+                            player.sendBlockChange(loc, createPortal(dir));
                             currentRenderedBlocks.add(block);
                         }
                     }
@@ -82,5 +101,21 @@ public class PortalRendererListener implements Listener {
                 break;
             }
         }
+    }
+
+    private BlockData createWallSign(int[] dir) {
+        Sign sign = (Sign) Material.OAK_WALL_SIGN.createBlockData();
+        if (dir == EAST) sign.setRotation(BlockFace.WEST);
+        if (dir == WEST) sign.setRotation(BlockFace.EAST);
+        if (dir == NORTH) sign.setRotation(BlockFace.SOUTH);
+        if (dir == SOUTH) sign.setRotation(BlockFace.NORTH);
+        return sign;
+    }
+
+    private BlockData createPortal(int[] dir) {
+        Orientable portal = (Orientable) Material.NETHER_PORTAL.createBlockData();
+        if (dir == EAST || dir == WEST) portal.setAxis(Axis.Z);
+        if (dir == NORTH || dir == SOUTH) portal.setAxis(Axis.X);
+        return portal;
     }
 }
